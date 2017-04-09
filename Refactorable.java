7cafc5550623da1aa41757b4351fd154e9adf0ee
abu jahid <abujahidroyal@gmail.com>
2017-04-08 20:37:03 -0700

    Extracting printBanner method

--- Refactorable.java
+++ Refactorable.java
@@ -1,9 +1,22 @@
- void printOwing() {
-  		}
+void printOwing() {
+  		double outstanding = 0.0;
   		
-  		// print details
- 		printDetail(outstanding);
-	}	public void printDetail(double outstanding) {
-  		System.out.println("Name : " + _name);
+  		// print banner
+ 		System.out.println("*************************");
+ 		System.out.println("***** Customer Owes *****");
+ 		System.out.println("*************************");
+		printBanner();
+  		
+  		// calculate outstanding
+  		while(e.hasMoreElements()){
+  void printOwing() {
   		System.out.println("Amount : " + outstanding);
   	}
+  
+ 	private void printBanner() {
+ 		System.out.println("*************************");
+ 		System.out.println("***** Customer Owes *****");
+ 		System.out.println("*************************");
+ 	}
+ 
+  }

b398d67580685ccf00a530ae5d7ad05daab18135
abu jahid <abujahidroyal@gmail.com>
2017-04-08 20:35:20 -0700

    Extracting printDetails method

--- Refactorable.java
+++ Refactorable.java
@@ -1,26 +1,9 @@
-void printOwing() {
-  		printBanner();
-  		
-  		// calculate outstanding
- 		outstanding = calculateOutstanding(e, outstanding);
- 		
- 		// print details
- 		printDetail(outstanding);
- 	}
- 
- 	public double calculateOutstanding(Enumeration<E> e, double outstanding) {
-  		while(e.hasMoreElements()){
-  			Order each = (Order) e.nextElement();
-  			outstanding += each.getAmount();
+ void printOwing() {
   		}
- 		
- 		// print details
+  		
+  		// print details
  		printDetail(outstanding);
- 		return outstanding;
+	}	public void printDetail(double outstanding) {
+  		System.out.println("Name : " + _name);
+  		System.out.println("Amount : " + outstanding);
   	}
-  
-  	public void printDetail(double outstanding) {
-   private void printBanner() {
-  	}
-  
-  }
